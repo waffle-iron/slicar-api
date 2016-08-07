8ee59ffd15f512266776a32f376069e24c5058f3
alejoescobar <alejoescobac@gmail.com>
2016-08-07 15:22:02 -0500

    Add initial routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json }, path: '/' do
+    # We are going to list our resources here
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+      resources :users, :only => [:show, :create, :update, :destroy] do
+        collection do
+          get 'reset_password'
+        end
+      end
+    end
+
+  end
+
 end
